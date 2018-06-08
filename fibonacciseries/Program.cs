d332133e993dfc592c0d1328b1f373572d8cc32e
Shishika <40016944+Shishika@users.noreply.github.com>
2018-06-08 11:25:30 +0530

    Add files via upload

---
+++ fibonacciseries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fibonaci
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop,first = 0,second = 1,third = 0;
+            int count;
+            Console.WriteLine("enter count value");
+            count = int.Parse(Console.ReadLine());
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            for (loop = 0; loop <= count; loop++)
+            {
+                third = first + second;
+                Console.WriteLine(third);
+                first = second;
+                second = third;
+            }
+            Console.ReadLine();
+         }
+    }
+}
